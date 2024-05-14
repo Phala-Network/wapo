d8d47379599399f16d1d8272cbbd1a2f080fbdee
Kevin Wang <wy721@qq.com>
2024-05-14 16:20:17 +0000

    todo: Add task for JWT authentication implementation

--- wapod/src/main.rs
+++ wapod/src/main.rs
@@ -35,6 +35,7 @@ async fn main() -> Result<()> {
     let todo = "Validate max_memory_pages * max_instances < WAPOD_ENCLAVE_MEM_SIZE";
     let todo = "Store instance logs to disk";
     let todo = "implement on-demand instance";
+    let todo = "implement JWT auth";
 
     info!("Starting wapod server...");
     let args = Args::parse();
