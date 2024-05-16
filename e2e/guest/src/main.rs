892f445f998b16f19e17c78527aff654dfffb0de
Kevin Wang <wy721@qq.com>
2024-05-16 11:47:09 +0800

    feat(e2e/guest): Add "/sleep" endpoint to handle sleep requests
    - Added a new "/sleep" endpoint to the match statement in main.
    - Implemented `handle_sleep` function to parse sleep duration and delay response.

--- e2e/guest/src/main.rs
+++ e2e/guest/src/main.rs
@@ -1,4 +1,6 @@
 #![no_main]
+use std::time::Duration;
+
 use log::{error, info};
 
 #[wapo::main]
@@ -17,10 +19,21 @@ async fn main() {
         let reply = match query.path.as_str() {
             "/echo" => query.payload,
             "/helloworld" => b"Hello, world!".to_vec(),
+            "/sleep" => handle_sleep(query.payload).await,
             _ => b"404".to_vec(),
         };
         if let Err(err) = query.reply_tx.send(&reply) {
             error!("Failed to send reply: {:?}", err);
         }
+    }
+}
+
+async fn handle_sleep(data: Vec<u8>) -> Vec<u8> {
+    match String::from_utf8_lossy(&data).parse() {
+        Ok(ms) => {
+            wapo::time::sleep(Duration::from_millis(ms)).await;
+            format!("Slept {ms} ms").into_bytes()
+        }
+        Err(_) => b"Invalid timestamp".to_vec(),
     }
 }
